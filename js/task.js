f2e35819b6f7f4f92c59db30f7316fcdd9d379d6
Elena Vlasenko <olefelena@gmail.com>
2021-03-09 16:57:22 +0200

    completer gallery close

--- js/task.js
+++ js/task.js
@@ -1,10 +1,19 @@
 import images from "./gallery-items.js";
 
-const galleryContainer = document.querySelector('.js-gallery')
+const refs = {
+    galleryContainer: document.querySelector('.js-gallery'),
+    lightbox: document.querySelector('.js-lightbox'),
+    btnLightboxClose: document.querySelector('[data-action="close-lightbox"]'),
+    lightboxImage: document.querySelector('.lightbox__image'),
+    lightboxOverlay: document.querySelector('.lightbox__overlay'),
+}
 const galleryMarkup = createGallery(images);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup)
-galleryContainer.addEventListener('click', handleGalleryItemClick)
+refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+refs.galleryContainer.addEventListener('click', handleGalleryItemClick);
+refs.btnLightboxClose.addEventListener('click', handleBtnCloseClick);
+refs.lightboxOverlay.addEventListener('click', handlelightboxOverlayClick);
 
 function createGallery(images) {
     return images.map(({ preview, original, description }) => {
@@ -32,5 +41,22 @@ function handleGalleryItemClick(evt) {
     if (!imageEl) {
         return;
     };
-console.log(evt.target.dataset.source);
+    openLightbox();
+    refs.lightboxImage.src = evt.target.dataset.source;
+    refs.lightboxImage.alt = evt.target.alt;
+}
+
+function openLightbox() {
+    refs.lightbox.classList.add('is-open');
+
+}
+
+function handleBtnCloseClick() {
+    refs.lightbox.classList.remove('is-open');
+    refs.lightboxImage.src = '';
+    refs.lightboxImage.alt = '';
+}
+
+function handlelightboxOverlayClick(evt) {
+    handleBtnCloseClick();
 }
